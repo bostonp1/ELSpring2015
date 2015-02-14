752e9cb0df3bd5f01558289feabc9f2659356390
bostonp1 <bostonp1@hawkmail.newpaltz.edu>
2015-02-14 17:49:23 -0500

    Code of temp logged in sqlite3

---
+++ code/log_Temp.py
@@ -0,0 +1,32 @@
+#/usr/bin/python
+
+import os
+import time
+import sqlite3
+import sys
+"""Log CurrentTime, Temperature in Celsius and Fahrenheit
+	 Returns a list [time, tempC, tempF]"""
+
+def readTemp():
+
+	tempfile=open("/sys/bus/w1/devices/28-00000698083b/w1_slave")
+	tempfile_text=tempfile.read()
+	currentTime=time.strftime('%x %X %Z')
+	tempfile.close()
+	tempC=float(tempfile_text.split("\n")[1].split("t=")[1])/1000
+	tempF=tempC*9.0/5.0+32.0
+	
+	params = (currentTime, tempC, tempF)
+	
+	con = sqlite3.connect('logTemp.db') 
+
+	with con:
+    
+   		 cur = con.cursor()
+   		 cur.execute("INSERT INTO tempData2 VALUES(?, ?, ?)", params)
+    
+   		 data = cur.fetchone()
+    
+	return[currentTime,tempC,tempF]
+print readTemp()
+print "Temperature logged"
